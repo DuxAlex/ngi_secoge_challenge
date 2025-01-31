1a1469a243060f904f48ff9e5d043ac93368938b
DuxAlex <alexkrypto.ti@gmail.com>
2025-01-31 20:51:15 -0300

    ajuste em algumas pastas, e teste do processo de transformacao usando extensão .py tudo ok

--- scripts/transformar_salvar.py
+++ scripts/transformar_salvar.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Caminho do arquivo CSV
-url_csv = "https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SRAG/2024/INFLUD24-20-01-2025.csv"
+caminho_csv = "../data/INFLUD24-20-01-2025.csv"
 
 # Carregar o CSV especificando o delimitador correto
-dados = pd.read_csv(url_csv, delimiter=';')
+dados = pd.read_csv(caminho_csv, delimiter=';')
 
 # Definir as colunas desejadas
 colunas_selecionadas = [
@@ -16,14 +16,18 @@ colunas_selecionadas = [
 dados_selecionados = dados[colunas_selecionadas]
 
 # Filtrar os dados onde SG_UF_NOT é "PE" ou "26"
-dados_pe_ou_26 = dados_selecionados[dados_selecionados["SG_UF_NOT"].isin(["PE", "26"])]
+dados_pe_ou_26 = dados_selecionados[
+    dados_selecionados["SG_UF_NOT"].isin(["PE", "26"])
+]
 
 # Excluir as linhas que possuam dados nulos ou vazios
 dados_limpos = dados_pe_ou_26.dropna()
 
+
 # Função para garantir que uma coluna seja convertida para inteiros
 def converter_para_inteiro(coluna):
     return pd.to_numeric(coluna, errors='coerce', downcast='integer')
+
 
 # Converter as colunas com valores decimais para inteiros
 colunas_a_converter = ["ESTRANG", "VACINA_COV", "CLASSI_FIN", "EVOLUCAO"]
